ab30de85e925b8c41b7bda39dc8210dd5836524d
tuyulbodo99 <122065701+tuyulbodo99@users.noreply.github.com>
2026-05-21 08:45:35 +0700

    fix: ganti hokagelegend9999 → tuyulbodo99/vpnscript

--- update.sh
+++ update.sh
@@ -27,13 +27,13 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/tuyulbodo99/vpnscript/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
     rm -rf menu.zip
     rm -rf update.sh
-    wget -qO- fv-tunnel "https://raw.githubusercontent.com/hokagelegend9999/alpha/main/config/fv-tunnel" 
+    wget -qO- fv-tunnel "https://raw.githubusercontent.com/tuyulbodo99/vpnscript/main/config/fv-tunnel" 
     chmod +x fv-tunnel 
     bash fv-tunnel
     rm -rf fv-tunnel
@@ -46,15 +46,15 @@ res1() {
     rm -rf menu-backup
     rm -rf backup
     rm -rf kontol
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/menu
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/m-sshws
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/addssh
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/addtr
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/addss
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/menu-backup
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/backup
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/regis
-    wget https://raw.githubusercontent.com/hokagelegend9999/alpha/main/menu/addhost
+    wget https://raw.githubusercontent.com/tuyulbodo99/vpnscript/main/menu/menu
+    wget https://raw.githubusercontent.com/tuyulbodo99/vpnscript/main/menu/m-sshws
+    wget https://raw.githubusercontent.com/tuyulbodo99/vpnscript/main/menu/addssh
+    wget https://raw.githubusercontent.com/tuyulbodo99/vpnscript/main/menu/addtr
+    wget https://raw.githubusercontent.com/tuyulbodo99/vpnscript/main/menu/addss
+    wget https://raw.githubusercontent.com/tuyulbodo99/vpnscript/main/menu/menu-backup
+    wget https://raw.githubusercontent.com/tuyulbodo99/vpnscript/main/menu/backup
+    wget https://raw.githubusercontent.com/tuyulbodo99/vpnscript/main/menu/regis
+    wget https://raw.githubusercontent.com/tuyulbodo99/vpnscript/main/menu/addhost
     chmod +x menu
     chmod +x m-sshws
     chmod +x addssh
